01c6414f7b7c259cbff534b461cb6b99712cc3af
zohreh shahmohammadi <shahmohamadizohreh@gmail.com>
2022-08-26 14:21:09 +0100

    first commit

---
+++ app/Models/Lesson.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+    public function comments(){
+        return $this->morphMany(Comment::class,'commentable');
+    }
+}
---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'body' => fake()->paragraphs(3,TRUE),
+            'lesson_id' => Lesson::all()->random()->id,
+        ];
+    }
+}
